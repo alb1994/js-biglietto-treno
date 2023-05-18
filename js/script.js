ad3ca4a3df64a5b316276f89c121a6eaf659d5d1
alb1994 <antonetti_alberto@libero.it>
2023-05-18 18:20:24 +0200

    aggiunta dichiarazine

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 //prompt km
 let chilometri = parseInt(prompt("inserisci i chilomentri da percorrere"));
 //prompt ceta
@@ -6,20 +7,20 @@ let eta = parseInt(prompt("inserisci la tua eta"));
 let costo_provvisorio = chilometri * 0.21;
 //condizionali
 if(eta < 18) {
-    risultato_scontato = costo_provvisorio * (20 / 100);
-    risultato_nf = costo_provvisorio - risultato_scontato;
+    let risultato_scontato = costo_provvisorio * (20 / 100);
+     risultato_nf = costo_provvisorio - risultato_scontato;
 }
 else if(eta > 65) {
-    risultato_scontato = costo_provvisorio * (40 / 100);
-    risultato_nf = costo_provvisorio - risultato_scontato;
+    let risultato_scontato = costo_provvisorio * (40 / 100);
+     risultato_nf = costo_provvisorio - risultato_scontato;
 }
 else if(eta <= 65){
-    risultato_scontato = costo_provvisorio * (0 / 100);
-    risultato_nf = costo_provvisorio - risultato_scontato;
+    let risultato_scontato = costo_provvisorio * (0 / 100);
+     risultato_nf = costo_provvisorio - risultato_scontato;
 }
 else if(eta >= 18){
-    risultato_scontato = costo_provvisorio * (0 / 100);
-    risultato_nf = costo_provvisorio - risultato_scontato;
+    let risultato_scontato = costo_provvisorio * (0 / 100);
+     risultato_nf = costo_provvisorio - risultato_scontato;
 }
 
 let risultato = risultato_nf.toFixed(2)
